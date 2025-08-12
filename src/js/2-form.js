2c1585cee45f970068fd3a776b86b65749c1546b
Victoria <victoria.vozyanovq@gmail.com>
2025-08-12 17:45:54 +0300

    add files

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,57 @@
+'use strict';
 
+let formData = { email: '', message: '' };
+
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
+
+function getFromLS(key, defaultValue) {
+  const jsonData = localStorage.getItem(key);
+  if (jsonData === null) {
+    return defaultValue;
+  }
+  try {
+    const data = JSON.parse(jsonData);
+    return data;
+  } catch {
+    return defaultValue || jsonData;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const formElem = document.querySelector('.feedback-form');
+
+  const lsData = getFromLS('feedback-form-state', { email: '', message: '' });
+
+  if (lsData && typeof lsData === 'object') {
+    formData.email = lsData.email;
+    formData.message = lsData.message;
+  } else {
+    formData.email = '';
+    formData.message = '';
+  }
+  formElem.elements.email.value = formData.email;
+  formElem.elements.message.value = formData.message;
+
+  formElem.addEventListener('input', e => {
+    formData.email = e.currentTarget.elements.email.value.trim();
+    formData.message = e.currentTarget.elements.message.value.trim();
+    saveToLS('feedback-form-state', formData);
+  });
+
+  formElem.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = e.currentTarget.elements.email.value;
+    const message = e.currentTarget.elements.message.value;
+    if (!email || !message) {
+      alert('Fill please all fields');
+      return;
+    }
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    formElem.reset();
+    formData = { email: '', message: '' };
+  });
+});
